571214f526fcc470b0359255a31ba40c707c3fec
Lauriefitz <lauriefitz98@outlook.ie>
2020-02-16 18:11:27 +0000

    iterate through S3

--- motion_detect_test2.py
+++ motion_detect_test2.py
@@ -38,76 +38,87 @@ def take_photo(bucket = "images-bucket-test-20075632"):
     newImage = ('/home/pi/final-year-project/image_%s.jpg' % i)
     print("A photo has been taken")
     camera.stop_preview()
-    object_name = ('image_%s.jpg' % i)
+    target_file = ('image_%s.jpg' % i)
     # Upload new image to S3 bucket
-    upload_file(newImage, bucket, object_name)
+    upload_file(newImage, bucket, target_file)
     sleep(10)
 
-def upload_file(newImage, bucket, object_name):
+def upload_file(newImage, bucket, target_file):
     # Create a connection with AWS
     s3_client = boto3.client('s3')
-    source_file = 'laurie.jpg'
     sleep(1)
     try:
         # Pushing file up to S3      (source file, bucket, target file)
-        response = s3_client.upload_file(newImage, bucket, object_name)
+        response = s3_client.upload_file(newImage, bucket, target_file)
         
-        sourceFile = source_file
-        targetFile = object_name
         # Run AWS Rekognition facial recognition algorithm
         # Compares source file with target file in the bucket
-        face_matches = compare_faces(sourceFile, targetFile, bucket)
+        face_matches = compare_faces(target_file, bucket)
         print("Face matches: " + str(face_matches))
         
         if(face_matches == 1):
-            print("It's " + source_file)
+            result = nameMatch
+            target = target_file
+            name_result, jpg = result.split('.')
+            name_target, jpg = target.split('.')
+            print(name_target + " is matching with " + name_result)
         else:
             print("No matches")
+        
     except ClientError as e:
         logging.error(e)
         return False
     return True
     
-def compare_faces(sourceFile, targetFile, bucket):
+def compare_faces(target_file, bucket):
     print("\nCompare Faces")
-    print("Source: " + sourceFile)
-    print("Target: " + targetFile)
-    print(bucket)
+    #print("Source: " + sourceFile)
+    print("Target: " + target_file)
+    #print(bucket)
+    
+    # Counter
+    j = 0
     
     # Create a connection with Rekognition
     client=boto3.client('rekognition')
     
     s3_connection = boto3.resource('s3')
     # Get source and target files from bucket
-    s3_object_source = s3_connection.Object(bucket, sourceFile)
-    s3_object_target = s3_connection.Object(bucket,targetFile)
-    s3_response_source = s3_object_source.get()
+    #s3_object_source = s3_connection.Object(bucket, sourceFile)
+    s3_object_target = s3_connection.Object(bucket,target_file)
+    
+    #s3_response_source = s3_object_source.get()
     s3_response_target = s3_object_target.get()
     
     # Read and open files
-    stream_source = io.BytesIO(s3_response_source['Body'].read())
+    #stream_source = io.BytesIO(s3_response_source['Body'].read())
     stream_target = io.BytesIO(s3_response_target['Body'].read())
-    imageSource = Image.open(stream_source)
+    #imageSource = Image.open(stream_source)
     imageTarget = Image.open(stream_target)
     
-    # AWS compare_faces algorithm
-    response = client.compare_faces(SimilarityThreshold=80,
-                                    SourceImage={'S3Object': {'Bucket':bucket, 'Name':sourceFile}},
-                                   TargetImage={'S3Object': {'Bucket':bucket, 'Name':targetFile}})
-    
-    for faceMatch in response['FaceMatches']:
-        position = faceMatch['Face']['BoundingBox']
-        similarity = str(faceMatch['Similarity'])
-        print("The face at " + str(position['Left']) + " " +
-              str(position['Top']) +
-              " matches with " + similarity + "% confidence")
+    # Declaring a non string bucket value
+    bucket1 = s3_connection.Bucket('images-bucket-test-20075632')
+    # Iterate through all objects (i.e images) in the bucket
+    for obj in bucket1.objects.all():
+        # Key = current image
+        key = obj.key
+        # AWS compare_faces algorithm, comparing current image with target image
+        response = client.compare_faces(SimilarityThreshold=80,
+                                        SourceImage={'S3Object': {'Bucket':bucket, 'Name':key}},
+                                        TargetImage={'S3Object': {'Bucket':bucket, 'Name':target_file}})
         
-        imageSource.show()
-        imageTarget.show()
-        imageSource.close()
-        imageTarget.close()
-        return len(response['FaceMatches'])
-    print("Done")
+        for faceMatch in response['FaceMatches']:
+            position = faceMatch['Face']['BoundingBox']
+            similarity = str(faceMatch['Similarity'])
+            global nameMatch
+            nameMatch = key
+            print(nameMatch + " The face at " + str(position['Left']) + " " +
+                  str(position['Top']) +
+                  " matches with " + similarity + "% confidence")
+            if len(response['FaceMatches'])==1:
+                j = j + 1
+            return j
+    return response['FaceMatches']
     
 # When motion detected begin 'take_photo' method
 pir.when_motion = take_photo
